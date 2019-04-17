a00bc6edc783744bc477b8434516db200ff0f511
tengyuan xie <xty1992a@gmail.com>
2019-04-17 08:16:23 +0800

    refactor:修复了线上iframeURL不正确的问题

--- src/views/Activity/MicroPage/children/Preview.vue
+++ src/views/Activity/MicroPage/children/Preview.vue
@@ -1,60 +1,72 @@
 <template>
   <div class="preview">
-    <iframe src="http://localhost:3001/mobile/#/MicroPage" frameborder="0" height="640" width="100%" ref="frame"></iframe>
+    <iframe
+      :src="previewPath"
+      frameborder="0"
+      height="640"
+      width="100%"
+      ref="frame"
+    ></iframe>
   </div>
 </template>
 
 <script>
-  export default {
-	name: 'Preview',
-	components: {},
-	props: {
-	  data: Array,
-	},
-	data() {
-	  return {}
-	},
-	mounted() {
-	  this.$refs.frame.onload = () => {
-		this.frame = this.$refs.frame.contentWindow || null
-	  }
-	},
-	methods: {},
-	computed: {},
-	watch: {
-	  data: {
-		handler(now) {
-		  if (!this.frame) return
-		  this.frame.postMessage({type: 'data-change', data: now}, '*')
-		}, deep: true,
-	  },
-	},
+export default {
+  name: "Preview",
+  components: {},
+  props: {
+    data: Array
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    this.$refs.frame.onload = () => {
+      this.frame = this.$refs.frame.contentWindow || null;
+    };
+  },
+  methods: {},
+  computed: {
+    previewPath() {
+      return process.env.NODE_ENV === "development"
+        ? "http://localhost:3001/mobile/#/MicroPage"
+        : "/mobile/#/MicroPage";
+    }
+  },
+  watch: {
+    data: {
+      handler(now) {
+        if (!this.frame) return;
+        this.frame.postMessage({ type: "data-change", data: now }, "*");
+      },
+      deep: true
+    }
   }
+};
 </script>
 
 <style lang="less" rel="stylesheet/less">
+.preview {
+  //.shadow;
+  width: 360px;
+  height: 640px;
+  overflow: hidden;
 
-  .preview {
-    //.shadow;
-    width: 360px;
-    height: 640px;
-    overflow: hidden;
-
-    iframe {
-      margin-right: -10px;
-    }
+  iframe {
+    margin-right: -10px;
   }
+}
 
-  .shadow {
-    position: relative;
+.shadow {
+  position: relative;
 
-    &:after {
-      content: '';
-      position: absolute;
-      bottom: 10px;
-      right: 10px;
-      top: 0;
-      left: 0;
-    }
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    top: 0;
+    left: 0;
   }
+}
 </style>
--- src/views/Activity/MicroPage/children/Tools/Carousel.vue
+++ src/views/Activity/MicroPage/children/Tools/Carousel.vue
@@ -1,94 +1,97 @@
 <template>
-  <div class="tool-carousel"  >
+  <div class="tool-carousel">
     <ul class="img-list">
-      <DelAble class="img-item" @del="delImg(it)"
-               v-for="it in data.data" :key="it.path">
+      <DelAble
+        class="img-item"
+        @del="delImg(it)"
+        v-for="it in data.data"
+        :key="it.path"
+      >
         <li>
-          <img :src="it.path" alt="">
+          <img :src="it.path" alt="" />
         </li>
       </DelAble>
       <li class="img-item add-btn" @click="pickCarousel">
-        <svg-icon icon="plus"/>
+        <svg-icon icon="plus" />
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-  import DelAble from '@/components/DelAble'
+import DelAble from "@/components/DelAble";
+import pickCarousel from "@/service/Micro/CarouselEditor";
 
-  const list = [...Array(6)].map((n, i) => ({
-	path: `http://localhost:3001/images/goods__${i + 1}.png`,
-	url: '',
-  }))
-  import pickCarousel from '@/service/Micro/CarouselEditor'
+const list = [...Array(6)].map((n, i) => ({
+  path: `/images/goods__${i + 1}.png`,
+  url: ""
+}));
 
-  export default {
-	name: 'Carousel',
-	components: {DelAble},
-	props: {
-	  data: Object,
-	  edit: Function,
-	},
-	methods: {
-	  async pickCarousel() {
-		let res = await pickCarousel({
-		  value: this.data.data || [],
-		  list,
-		})
-		if (res.success) {
-		  this.edit && this.edit({
-			...this.data,
-			data: res.data,
-		  })
-		}
-		console.log(res)
+export default {
+  name: "Carousel",
+  components: { DelAble },
+  props: {
+    data: Object,
+    edit: Function
+  },
+  methods: {
+    async pickCarousel() {
+      let res = await pickCarousel({
+        value: this.data.data || [],
+        list
+      });
+      if (res.success) {
+        this.edit &&
+          this.edit({
+            ...this.data,
+            data: res.data
+          });
+      }
+      console.log(res);
+    },
 
-	  },
-
-	  delImg(item) {
-		this.edit && this.edit({
-		  ...this.data,
-		  data: this.data.data.filter(it => it.path !== item.path),
-		})
-	  },
-	},
-	computed: {},
+    delImg(item) {
+      this.edit &&
+        this.edit({
+          ...this.data,
+          data: this.data.data.filter(it => it.path !== item.path)
+        });
+    }
   }
+};
 </script>
 
 <style lang="less" rel="stylesheet/less">
+.tool-carousel {
+  .img-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -4px;
 
-  .tool-carousel {
-    .img-list {
-      display: flex;
-      flex-wrap: wrap;
-      margin: 0 -4px;
+    .img-item {
+      width: 120px;
+      height: 75px;
+      margin: 4px;
 
-      .img-item {
-        width: 120px;
-        height: 75px;
-        margin: 4px;
-
-        img {
-          width: 100%;
-          height: 100%;
-        }
+      img {
+        width: 100%;
+        height: 100%;
       }
+    }
 
-      .add-btn {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 50px;
-        border: 1px dashed #e5e5e5;
-        cursor: pointer;
+    .add-btn {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 50px;
+      border: 1px dashed #e5e5e5;
+      cursor: pointer;
 
-        span {
-          vertical-align: middle;
-          line-height: 1;
-        }
+      span {
+        vertical-align: middle;
+        line-height: 1;
       }
     }
   }
+}
 </style>
