304827d6d90d715c4208132e14fa051fbdd3510c
谢腾远 <redbuck@126.com>
2019-11-21 11:07:41 +0800

    测试vpsGit脚本

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import router from "./router/index.js";
 import store from "./store/index.js";
@@ -18,7 +16,6 @@ Vue.config.productionTip = false;
 Vue.prototype.$storage = storage;
 Vue.use(CacheTagData);
 
-/* eslint-disable no-new */
 new Vue({
   el: "#app",
   router,
